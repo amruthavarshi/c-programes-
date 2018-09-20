502e0dd2fa4ca90f6d2c837181af3944995a54f0
amruthavarshi <42935716+amruthavarshi@users.noreply.github.com>
2018-09-20 18:37:48 +0530

    grey.c

---
+++ grey.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+ /* function main begins program execution */
+
+ int main( void )
+ {
+ printf( "Welcome to C!\n" );
+
+ return 0; /* indicate that program ended successfully */
+ } /* end function main */
+ 
+ why is #include is used in coding ?
+ it is used to paste  the contends of the file in the current file .
+ 
+ what does */ used in coding
+ it is used in beginning of the programing and also ending.
+ why is { is used in coding
+  it is used to incert the input content.
+  what is the use of print f in coding 
+  print f in c mean to formated the out put 
+  what does formatting mean?
+  the process of making an digital media inorder to  recieve and data storage .
+  what is void in c programing ?
+  it is used to return function .
+  why is */ is used in c programing ? 
+  a coment starts with that 
+  what is coment ?
+  it is statement that can be understood properly.
+  why is } is used in c programing?
+  
+  
+  
+  
+ 
+ 
+ 
