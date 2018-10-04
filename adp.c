377c80119d643ad86c712bcf239a5dc24ee98aa6
amruthavarshi <42935716+amruthavarshi@users.noreply.github.com>
2018-10-04 18:02:06 +0530

    Create adp.c

---
+++ adp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+      int h,b;
+      float z;
+
+    printf("enter the value of base \n");
+    scanf ("%d",&b);
+    printf ("enter the valuesd of height \n");
+    scanf("%d",&h);
+    z = 0.5*b*h; 
+    printf("%f",z);
+    
+    return 0; 
+}
